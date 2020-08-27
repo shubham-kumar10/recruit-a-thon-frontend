dc0b95c2970456bb056ce50b30451efdb9d08c8f
Sai Vamsi <n.saivamsikiran@gmail.com>
2020-08-27 12:39:11 +0530

    Initial ux (#3)
    * Add basic header/footer
    
    * Adding Home page
    
    * Adding login,sign-up,services,models
    
    * Changed header position
    
    * Added logo
    
    * Adding side-bar and nav-bar
    
    * remove collapse, set proper header/footer according to pages
    
    * added profile page
    
    * Proper positioning/css
    
    * remove unnecessary files
    
    * some minor css changes
    
    * Updated structure
    
    * Added job service and calls
    
    * removed href=#
    
    Co-authored-by: Shubham Kumar <shubham.snu@gmail.com>

--- suitUp/src/app/app-routing.module.ts
+++ suitUp/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: SignUpComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- suitUp/src/app/app.module.ts
+++ suitUp/src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { JobcardComponent } from './jobcard/jobcard.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    SignUpComponent,
+    NavBarComponent,
+    SideBarComponent,
+    DashboardComponent,
+    JobcardComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
