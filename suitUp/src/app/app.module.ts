a18ed0080f7ef834274f2172e63a7f1956a7f265
Shubham Kumar <shubham.snu@gmail.com>
2020-08-28 15:48:57 +0530

    Test3 (#5)
    * css to scss
    
    * Structure changes
    
    * Structure changes
    
    Co-authored-by: Sai Vamsi Kiran <n.saivamsikiran@gmail.com>

--- suitUp/src/app/app.module.ts
+++ suitUp/src/app/app.module.ts
@@ -28,7 +28,7 @@ import { ProfileComponent } from './profile/profile.component';
     SideBarComponent,
     DashboardComponent,
     JobcardComponent,
-    ProfileComponent
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
